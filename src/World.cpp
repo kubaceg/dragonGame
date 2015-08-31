bda1f570d95d95dc5b976563ae65ff78b8f89d12
Kuba <kuba.ceg@gmail.com>
2015-08-31 21:13:03 +0200

    World background

--- src/World.cpp
+++ src/World.cpp
@@ -1,13 +1,23 @@
 #include "World.h"
 
-World::World(SDL_Surface *sc)
-{
+World::World(SDL_Surface *sc) {
     screen = sc;
     frameCount = 0;
-    clouds = new Clouds(sc);
+    background = IMG_Load("graph/cloudsbg.png");
+
+    bgPosition.x = 0;
+    bgPosition.y = 0;
+    bgPosition.w = 640;
+    bgPosition.h = 480;
 }
 
-void World::draw()
-{
-    clouds->draw();
+void World::draw() {
+    frameCount++;
+
+    if (frameCount == 10) {
+        bgPosition.x += 1;
+        frameCount = 0;
+    }
+
+    SDL_BlitSurface(background, &bgPosition, screen, NULL);
 }
--- src/World.h
+++ src/World.h
@@ -1,17 +1,20 @@
 #ifndef __WORLD_H__
 #define __WORLD_H__
+
 #include "SDL.h"
 #include "Clouds.h"
 
-class World
-{
+class World {
 private:
-    SDL_Surface* screen;
+    SDL_Surface *screen;
+    SDL_Surface *background;
+    SDL_Rect bgPosition;
     int frameCount;
-    Clouds *clouds;
 
 public:
     void draw();
-    World(SDL_Surface* sc);
+
+    World(SDL_Surface *sc);
 };
+
 #endif
